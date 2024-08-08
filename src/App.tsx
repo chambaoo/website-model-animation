68d13677f2817c374ca8d72865cf83fae4743854
chambaoo <105230591+chambaoo@users.noreply.github.com>
2024-08-08 02:14:21 +0000

    add: (working on header)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,21 @@ function App() {
 
   return (
     <>
-      <div className='h-screen flex items-center justify-center'>
+      <div className='h-screen bg-slate-900'>
+        {/* header */}
+        <header className='flex text-white'>
+          <span>Website Model Animation</span>
+          <nav>
+            <ul>
+              <li>
+                <a href='/'>Home</a>
+              </li>
+              <li>
+                <a href='#about'>About</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <motion.div
           initial={{ x: 100 }}
           animate={{ x: 0 }}
